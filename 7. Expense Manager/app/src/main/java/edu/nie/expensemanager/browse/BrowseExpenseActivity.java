a416a6b1f382ee4a89b64353c135fea662b3fcc4
Aditya Sharat <aditya.sharat@flipkart.com>
2017-03-17 17:26:16 +0530

    make packages feature based

--- 7. Expense Manager/app/src/main/java/edu/nie/expensemanager/browse/BrowseExpenseActivity.java
+++ 7. Expense Manager/app/src/main/java/edu/nie/expensemanager/browse/BrowseExpenseActivity.java
@@ -1,4 +1,20 @@
-package edu.nie.expensemanager;
+/*
+ * Copyright 2016 Flipkart Internet Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.nie.expensemanager.browse;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,7 +24,9 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import edu.nie.expensemanager.R;
+
+public class BrowseExpenseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- 7. Expense Manager/app/src/main/java/edu/nie/expensemanager/browse/ExpenseListAdapter.java
+++ 7. Expense Manager/app/src/main/java/edu/nie/expensemanager/browse/ExpenseListAdapter.java
@@ -1,4 +1,20 @@
-package edu.nie.expensemanager;
+/*
+ * Copyright 2016 Flipkart Internet Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.nie.expensemanager.browse;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
@@ -10,6 +26,7 @@ import android.view.ViewGroup;
 
 import java.util.List;
 
+import edu.nie.expensemanager.R;
 import edu.nie.expensemanager.models.Expense;
 
 /**
