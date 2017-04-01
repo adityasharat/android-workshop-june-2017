79a0371eb7cabdeccd65ca6d696ba94a7b8a6edb
Vinay <s.vinay.rao@gmail.com>
2017-04-01 14:58:09 +0530

    Initial commit for Layout
    Initial commit for Layout

--- 2. Layout/src/main/java/edu/nie/layouts/MainActivity.java
+++ 2. Layout/src/main/java/edu/nie/layouts/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.nie.layouts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent (getApplicationContext(), BasicActivity.class) ;
+                startActivity(intent1);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent (getApplicationContext(), LinearLayoutHorizontal.class) ;
+                startActivity(intent2);
+            }
+        });
+
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent (getApplicationContext(), LinearLayoutVertical.class) ;
+                startActivity(intent3);
+            }
+        });
+
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent4 = new Intent (getApplicationContext(), RelativeLayoutSample.class) ;
+                startActivity(intent4);
+            }
+        });
+
     }
 }
