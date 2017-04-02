e14db01d9e121078288f37b43c5ac8a8028842ed
Vinay S <s.vinay.rao@gmail.com>
2017-04-02 12:15:52 +0530

    UI Interactions samples Implemented

---
+++ 3. Interactions/src/main/java/edu/nie/interactions/EditAcitivity.java
@@ -0,0 +1,30 @@
+package edu.nie.interactions;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+public class EditAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_default);
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.layout1) ;
+
+        EditText editText = new EditText(getApplicationContext()) ;
+        editText.setHint(getResources().getString(R.string.entername));
+        editText.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        editText.setTextColor(Color.parseColor("#3333cc"));
+        editText.setTextSize(25);
+        editText.setTypeface(Typeface.SANS_SERIF);
+        editText.setInputType(InputType.TYPE_CLASS_TEXT |InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        linearLayout.addView(editText);
+    }
+}
--- 3. Interactions/src/main/java/edu/nie/interactions/MainActivity.java
+++ 3. Interactions/src/main/java/edu/nie/interactions/MainActivity.java
@@ -1,13 +1,74 @@
 package edu.nie.interactions;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.layout1) ;
+
+        ListView listView = (ListView) findViewById(R.id.listview) ;
+
+        ArrayList <String> listActivities = new ArrayList<>() ;
+
+        listActivities.add("Text View activity") ;
+        listActivities.add("Edit View activity") ;
+        listActivities.add("Button View activity") ;
+        listActivities.add("Toggle Button activity") ;
+        listActivities.add("Check Box activity") ;
+        listActivities.add("Radio Button activity") ;
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listActivities) ;
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0 :
+                        startActivity(new Intent(getApplicationContext(),TextViewActivity.class));
+                        break;
+                    case 1:
+                        startActivity(new Intent(getApplicationContext(),EditAcitivity.class));
+                        break;
+                    case 2:
+                        startActivity(new Intent(getApplicationContext(),ButtonActivity.class));
+                        break;
+                    case 3:
+                        startActivity(new Intent(getApplicationContext(),ToggleButtonAcitivity.class));
+                        break;
+                    case 4:
+                        startActivity(new Intent(getApplicationContext(),CheckBoxActivity.class));
+                        break;
+                    case 5:
+                        startActivity(new Intent(getApplicationContext(),RadioActivity.class));
+                        break;
+                }
+            }
+        });
+
     }
 }
