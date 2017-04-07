4c08798eeb766a2b874faf229d2774371ffb3ea6
Aditya Sharat <aditya.sharat@flipkart.com>
2017-04-07 13:52:11 +0530

    complete the async task tutorial

--- 6. AsyncTask And Network/src/main/java/edu/nie/asynctaskandnetwork/MainActivity.java
+++ 6. AsyncTask And Network/src/main/java/edu/nie/asynctaskandnetwork/MainActivity.java
@@ -1,13 +1,97 @@
 package edu.nie.asynctaskandnetwork;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final View startTaskView = findViewById(R.id.start_task);
+        final TextView statusView = (TextView) findViewById(R.id.run_status);
+        final CheckBox runAsyncView = (CheckBox) findViewById(R.id.run_async);
+
+        TextView incrementView = (TextView) findViewById(R.id.increment);
+        final TextView counterView = (TextView) findViewById(R.id.counter);
+
+        startTaskView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                statusView.setText("Running...");
+
+                if (runAsyncView.isChecked()) {
+
+                    v.setEnabled(false);
+
+                    Log.d("order", "1");
+                    startSuperComplexTaskInBackground(v, statusView);
+                    Log.d("order", "3");
+
+                } else {
+
+                    v.setEnabled(false);
+
+                    Log.d("order", "1");
+                    startSuperComplexTask();
+                    Log.d("order", "3");
+
+                    v.setEnabled(true);
+                    statusView.setText("Completed !");
+                }
+
+            }
+        });
+
+        incrementView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                counterView.setText(String.valueOf(counter));
+            }
+        });
+
+    }
+
+    private void startSuperComplexTask() {
+        Log.d("order", "2");
+        for (int i = 0; i < 100000; i++) {
+            isPrime(i);
+        }
+    }
+
+    private boolean isPrime(int x) {
+        for (int f = 2; f < x; f++) {
+            if (x % f == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void startSuperComplexTaskInBackground(final View button, final TextView statusView) {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                startSuperComplexTask();
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                button.setEnabled(true);
+                statusView.setText("Completed !");
+            }
+        }.execute();
     }
 }
