d2b95528ab719994c0c57723e95b5caea5813b63
Aditya Sharat <aditya.sharat@flipkart.com>
2017-04-09 10:38:19 +0530

    add comments

--- 7. Expense Manager/src/main/java/edu/nie/expensemanager/browse/BrowseExpenseActivity.java
+++ 7. Expense Manager/src/main/java/edu/nie/expensemanager/browse/BrowseExpenseActivity.java
@@ -26,29 +26,59 @@ public class BrowseExpenseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // the layout to inflate
         setContentView(R.layout.activity_main);
 
+        // find the tool bar from the above layout and assign it to toolbar
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        // this method is from AppCompatActivity
+        // used to support older Android OS versions
+        // which did not have this fancy new toolbar
+        // pre bundled
         setSupportActionBar(toolbar);
 
+        // find the floating `+` button and assign it to fab
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_expense_button);
+
+        // set a click listener so you can do
+        // something when the fab button is clicked
+        // in this case open the editor
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // open a new editor
+                // for a new expense
+                // hence null
                 openExpenseEditor(null);
             }
         });
 
+        // FORGET THIS FOR NOW
+        // This is a super optimised View Group
+        // to render scrollable views with millions
+        // of rows.
         final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.expense_recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        // This is a listener that we made, which we will use
+        // with the above Recycler View
+        // its is an interface. Like public class Car in C++
         BrowseExpenseListener listener = new BrowseExpenseListener() {
             @Override
             public void openExpense(@Nullable Expense expense) {
+
+                // When the user taps on any row or expense
+                // We want to open that expense for editing
+                // The callback is giving you that expense
+                // Just open the activity to edit
                 openExpenseEditor(expense);
+
             }
         };
 
+        // IGNORE ALL THIS FOR NOW
         adapter = new ExpenseListAdapter(listener);
         recyclerView.addItemDecoration(new VerticalSpace(12));
         recyclerView.setAdapter(adapter);
@@ -57,12 +87,28 @@ public class BrowseExpenseActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        // This load method starts an Async Task
+        // Reads all the Expenses from the DB
+        // and sets it in that Recycler View thing.
         load(adapter);
     }
 
+    /**
+     * This Method navigates to the expense editor page
+     * If you pass null as the arguments it will open an empty page
+     * If you pass a valid Expense, It will open that expense to edit
+     */
     private void openExpenseEditor(@Nullable Expense expense) {
+        // ExpenseEditorActivity is the page we want to open
         Intent intent = new Intent(BrowseExpenseActivity.this, ExpenseEditorActivity.class);
+
+        // we are adding some data to the navigation intent
+        // This is the id of you expense if it is not null
+        // Else Expense.NO_ID == -1;
+        // ExpenseEditorActivity.KEY_EXPENSE is just a string identifier
         intent.putExtra(ExpenseEditorActivity.KEY_EXPENSE, expense != null ? expense.id : Expense.NO_ID);
+
         startActivity(intent);
     }
 
--- 7. Expense Manager/src/main/java/edu/nie/expensemanager/editor/ExpenseEditorActivity.java
+++ 7. Expense Manager/src/main/java/edu/nie/expensemanager/editor/ExpenseEditorActivity.java
@@ -30,10 +30,12 @@ public class ExpenseEditorActivity extends AppCompatActivity {
 
     public static final String KEY_EXPENSE = "exp";
 
-    EditText descriptionView;
-    EditText amountView;
-    TextView dateView;
-    EditText titleView;
+    // These are the fields we are talking
+    // about below. Just like C++ class memebers
+    private EditText descriptionView;
+    private EditText amountView;
+    private TextView dateView;
+    private EditText titleView;
 
     private long id = Expense.NO_ID;
     private boolean isEditFlow;
@@ -46,15 +48,31 @@ public class ExpenseEditorActivity extends AppCompatActivity {
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // Get the id we passed in the intent
+        // Use the same String KEY_EXPENSE.
+        // the second argument is basically
+        // what should be returned if KEY_EXPENSE
+        // was not even present in the intent data
         id = getIntent().getLongExtra(KEY_EXPENSE, Expense.NO_ID);
 
+        // Find all the view we need from the activity_expense_editor layout
+        // These are fields not variables
         titleView = (EditText) findViewById(R.id.title);
         descriptionView = (EditText) findViewById(R.id.description);
         amountView = (EditText) findViewById(R.id.amount);
         dateView = (TextView) findViewById(R.id.date);
 
+        // Lets get the current time in ms
+        // see https://en.wikipedia.org/wiki/Unix_time to understand
+        // which is a long value
         long time = System.currentTimeMillis();
+
+        // Lets convert that into a human read able form
+        // and set it on the data view.
         dateView.setText(Utils.toDateString(time, null));
+
+        // Also lets save the epoch time inside the dateView
+        // So that we can get it when we are going to save the expense.
         dateView.setTag(time);
     }
 
@@ -68,12 +86,14 @@ public class ExpenseEditorActivity extends AppCompatActivity {
         }
     }
 
+    // IGNORE
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         outState.putLong(KEY_EXPENSE, id);
         super.onSaveInstanceState(outState);
     }
 
+    // IGNORE
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
@@ -82,15 +102,28 @@ public class ExpenseEditorActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Lets declare what should be the menu on the top
+     * Right of the screen.
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.editor_menu, menu);
         return true;
     }
 
+    /**
+     * Lets define what we will do when user clicks
+     * on any item of the menu.
+     * @param item
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            // If user clicks on the delete button
             case R.id.delete:
                 if (isEditFlow) {
                     delete(id);
@@ -103,25 +136,50 @@ public class ExpenseEditorActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Called when the user clicks on the Calendar Button
+     * @param v
+     */
     public void onDateSelect(View v) {
+        // Get the current date, month and year
         final Calendar c = Calendar.getInstance();
         int mYear = c.get(Calendar.YEAR);
         int mMonth = c.get(Calendar.MONTH);
         int mDay = c.get(Calendar.DAY_OF_MONTH);
 
+        // This is an in build class from Android which can be used to
+        // show a floating dialog to pick a date. It takes 2 arguments
+        // The page where you want to show the dialog (the activity)
+        // and a listener which has a method `onDateSet` which is called
+        // when user clicks on the `OK` button on the dialog
         DatePickerDialog datePickerDialog = new DatePickerDialog(ExpenseEditorActivity.this, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                // Lets convert the human readable date
+                // back to epoch time in milliseconds.
                 Calendar temp = Calendar.getInstance();
                 temp.set(year, monthOfYear, dayOfMonth);
                 long value = temp.getTimeInMillis();
+
+                // Lets set the human readable form into the dateView
                 dateView.setText(Utils.toDateString(value, null));
+
+                // Also lets save the epoch time inside the dateView
+                // So that we can get it when we are going to save the expense.
                 dateView.setTag(value);
             }
         }, mYear, mMonth, mDay);
+
+        // Show this dialog to the user
         datePickerDialog.show();
     }
 
+    /**
+     * Called when the user clicks on the
+     * save button.
+     * @param v
+     */
     public void onAddExpense(View v) {
         String title = titleView.getText().toString().trim();
         if (title.length() == 0) {
@@ -136,6 +194,8 @@ public class ExpenseEditorActivity extends AppCompatActivity {
         String description = descriptionView.getText().toString().trim();
         double amount = Double.parseDouble(string);
 
+        // Get the epoch time from the date view
+        // we saved it earlier
         long date = (long) dateView.getTag();
 
         Expense expense;
@@ -147,6 +207,11 @@ public class ExpenseEditorActivity extends AppCompatActivity {
         save(expense);
     }
 
+    /**
+     * Starts an Async Task to load a single Expense
+     * with the id passed in the arguments
+     * @param id
+     */
     private void load(final long id) {
         new AsyncTask<Context, Void, Cursor>() {
 
@@ -164,6 +229,10 @@ public class ExpenseEditorActivity extends AppCompatActivity {
         }.execute(this);
     }
 
+    /**
+     * Starts an Async Task to save a single Expense
+     * @param expense
+     */
     private void save(@NonNull final Expense expense) {
         new AsyncTask<Context, Void, Void>() {
             @Override
@@ -194,6 +263,11 @@ public class ExpenseEditorActivity extends AppCompatActivity {
         }.execute(this);
     }
 
+    /**
+     * Starts an Async Task to delete a single Expense with
+     * passed id
+     * @param id
+     */
     private void delete(final long id) {
         new AsyncTask<Context, Void, Void>() {
 
@@ -212,6 +286,11 @@ public class ExpenseEditorActivity extends AppCompatActivity {
         }.execute(this);
     }
 
+    /**
+     * Given an expense
+     * Set it on a editor page
+     * @param expense
+     */
     private void setExpense(@NonNull Expense expense) {
         titleView.setText(expense.title);
         descriptionView.setText(expense.description);
